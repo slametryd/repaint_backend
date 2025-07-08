690a4410c6fc0d35ce6ae9b0f3350c4e3e19c08f
Slametryd <slametryd812@gmail.com>
2025-07-09 05:17:30 +0700

    ngedeploy pagi pagi

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,23 @@
 import { Sequelize } from "sequelize";
 
+const isProduction = process.env.NODE_ENV === "production";
+
+// Pilih host & port otomatis:
+const dbHost = isProduction
+  ? process.env.DB_HOST_PROD
+  : process.env.DB_HOST_DEV;
+const dbPort = isProduction
+  ? process.env.DB_PORT_PROD
+  : process.env.DB_PORT_DEV;
+
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: dbHost,
+    port: dbPort,
     dialect: "mysql",
-    port: process.env.DB_PORT,
   }
 );
 
